d6010bcbf79b738b56486130714668ad57a57d74
allen.w <koomoxs@gmail.com>
2020-01-02 01:29:04 +0800

    Create tls.go

---
+++ tls.go
@@ -0,0 +1,28 @@
+package letsencrypt
+
+import "crypto/tls"
+
+func LoadTLSConfig(certFile, keyFile string) (tlsCfg *tls.Config) {
+	tlsCfg = &tls.Config{}
+	tlsCfg.MinVersion = tls.VersionTLS12
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		return nil
+	}
+	tlsCfg.Certificates = []tls.Certificate{cert}
+	tlsCfg.PreferServerCipherSuites = true
+	tlsCfg.CurvePreferences = []tls.CurveID{tls.CurveP256, tls.X25519}
+	tlsCfg.RootCAs = CertPool()
+	tlsCfg.CipherSuites = []uint16{
+		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+		tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, // Go 1.8 only
+		tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,   // Go 1.8 only
+		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+	}
+
+	return
+}
