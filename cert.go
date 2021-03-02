3c352fa396978a9c1e3817acaf41c2c25f984dd8
koomox <koomoxs@gmail.com>
2021-03-02 21:16:57 +0800

    Update cert.go

--- cert.go
+++ cert.go
@@ -507,6 +507,50 @@ h0VMhrl6ine7mG0UVBuIg9CLAf+cHCVsDyUiO8x50e5CNEjxh1hxgo46szLU5tHP
 aocuXuOLVuLdUat+j/2satKSUStQRigSXAp8fAd5oUXQhSh7GVtIf5eEvGvl04Np
 u70vXLDE2hdzTg==
 -----END CERTIFICATE-----`)
+	DigiCertBaltimoreRoot = []byte(`-----BEGIN CERTIFICATE-----
+MIIDdzCCAl+gAwIBAgIEAgAAuTANBgkqhkiG9w0BAQUFADBaMQswCQYDVQQGEwJJ
+RTESMBAGA1UEChMJQmFsdGltb3JlMRMwEQYDVQQLEwpDeWJlclRydXN0MSIwIAYD
+VQQDExlCYWx0aW1vcmUgQ3liZXJUcnVzdCBSb290MB4XDTAwMDUxMjE4NDYwMFoX
+DTI1MDUxMjIzNTkwMFowWjELMAkGA1UEBhMCSUUxEjAQBgNVBAoTCUJhbHRpbW9y
+ZTETMBEGA1UECxMKQ3liZXJUcnVzdDEiMCAGA1UEAxMZQmFsdGltb3JlIEN5YmVy
+VHJ1c3QgUm9vdDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKMEuyKr
+mD1X6CZymrV51Cni4eiVgLGw41uOKymaZN+hXe2wCQVt2yguzmKiYv60iNoS6zjr
+IZ3AQSsBUnuId9Mcj8e6uYi1agnnc+gRQKfRzMpijS3ljwumUNKoUMMo6vWrJYeK
+mpYcqWe4PwzV9/lSEy/CG9VwcPCPwBLKBsua4dnKM3p31vjsufFoREJIE9LAwqSu
+XmD+tqYF/LTdB1kC1FkYmGP1pWPgkAx9XbIGevOF6uvUA65ehD5f/xXtabz5OTZy
+dc93Uk3zyZAsuT3lySNTPx8kmCFcB5kpvcY67Oduhjprl3RjM71oGDHweI12v/ye
+jl0qhqdNkNwnGjkCAwEAAaNFMEMwHQYDVR0OBBYEFOWdWTCCR1jMrPoIVDaGezq1
+BE3wMBIGA1UdEwEB/wQIMAYBAf8CAQMwDgYDVR0PAQH/BAQDAgEGMA0GCSqGSIb3
+DQEBBQUAA4IBAQCFDF2O5G9RaEIFoN27TyclhAO992T9Ldcw46QQF+vaKSm2eT92
+9hkTI7gQCvlYpNRhcL0EYWoSihfVCr3FvDB81ukMJY2GQE/szKN+OMY3EU/t3Wgx
+jkzSswF07r51XgdIGn9w/xZchMB5hbgF/X++ZRGjD8ACtPhSNzkE1akxehi/oCr0
+Epn3o0WC4zxe9Z2etciefC7IpJ5OCBRLbf1wbWsaY71k5h+3zvDyny67G7fyUIhz
+ksLi4xaNmjICq44Y3ekQEe5+NauQrz4wlHrQMz2nZQ/1/I6eYs9HRCwBXbsdtTLS
+R9I4LtD+gdwyah617jzV/OeBHRnDJELqYzmp
+-----END CERTIFICATE-----`)
+	CloudflareIncECCCA3 = []byte(`-----BEGIN CERTIFICATE-----
+MIIDzTCCArWgAwIBAgIQCjeHZF5ftIwiTv0b7RQMPDANBgkqhkiG9w0BAQsFADBa
+MQswCQYDVQQGEwJJRTESMBAGA1UEChMJQmFsdGltb3JlMRMwEQYDVQQLEwpDeWJl
+clRydXN0MSIwIAYDVQQDExlCYWx0aW1vcmUgQ3liZXJUcnVzdCBSb290MB4XDTIw
+MDEyNzEyNDgwOFoXDTI0MTIzMTIzNTk1OVowSjELMAkGA1UEBhMCVVMxGTAXBgNV
+BAoTEENsb3VkZmxhcmUsIEluYy4xIDAeBgNVBAMTF0Nsb3VkZmxhcmUgSW5jIEVD
+QyBDQS0zMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEua1NZpkUC0bsH4HRKlAe
+nQMVLzQSfS2WuIg4m4Vfj7+7Te9hRsTJc9QkT+DuHM5ss1FxL2ruTAUJd9NyYqSb
+16OCAWgwggFkMB0GA1UdDgQWBBSlzjfq67B1DpRniLRF+tkkEIeWHzAfBgNVHSME
+GDAWgBTlnVkwgkdYzKz6CFQ2hns6tQRN8DAOBgNVHQ8BAf8EBAMCAYYwHQYDVR0l
+BBYwFAYIKwYBBQUHAwEGCCsGAQUFBwMCMBIGA1UdEwEB/wQIMAYBAf8CAQAwNAYI
+KwYBBQUHAQEEKDAmMCQGCCsGAQUFBzABhhhodHRwOi8vb2NzcC5kaWdpY2VydC5j
+b20wOgYDVR0fBDMwMTAvoC2gK4YpaHR0cDovL2NybDMuZGlnaWNlcnQuY29tL09t
+bmlyb290MjAyNS5jcmwwbQYDVR0gBGYwZDA3BglghkgBhv1sAQEwKjAoBggrBgEF
+BQcCARYcaHR0cHM6Ly93d3cuZGlnaWNlcnQuY29tL0NQUzALBglghkgBhv1sAQIw
+CAYGZ4EMAQIBMAgGBmeBDAECAjAIBgZngQwBAgMwDQYJKoZIhvcNAQELBQADggEB
+AAUkHd0bsCrrmNaF4zlNXmtXnYJX/OvoMaJXkGUFvhZEOFp3ArnPEELG4ZKk40Un
++ABHLGioVplTVI+tnkDB0A+21w0LOEhsUCxJkAZbZB2LzEgwLt4I4ptJIsCSDBFe
+lpKU1fwg3FZs5ZKTv3ocwDfjhUkV+ivhdDkYD7fa86JXWGBPzI6UAPxGezQxPk1H
+goE6y/SJXQ7vTQ1unBuCJN0yJV0ReFEQPaA1IwQvZW+cwdFD19Ae8zFnWSfda9J1
+CZMRJCQUzym+5iPDuI9yP+kHyCREU3qzuWFloUwOxkgAyXVjBYdwRVKD05WdRerw
+6DEdfgkfCv4+3ao8XnTSrLE=
+-----END CERTIFICATE-----`)
 )
 
 func loadCertPool(certsPem ...[]byte) (pool *x509.CertPool) {
@@ -528,7 +572,7 @@ func loadCertPool(certsPem ...[]byte) (pool *x509.CertPool) {
 }
 
 func CertPool() *x509.CertPool {
-	return loadCertPool(isrgrootx1, isrgrootx2, isrgrootx2crosssigned, letsencryptr3, letsencryptr3crosssigned, letsencrypte1, letsencryptr4, letsencryptr4crosssigned, letsencrypte2, letsencryptauthorityx1, letsencryptx1crosssigned, letsencryptauthorityx2, letsencryptx2crosssigned, letsencryptauthorityx3, letsencryptx3crosssigned, letsencryptauthorityx4, letsencryptx4crosssigned, isrgrootocspx1)
+	return loadCertPool(isrgrootx1, isrgrootx2, isrgrootx2crosssigned, letsencryptr3, letsencryptr3crosssigned, letsencrypte1, letsencryptr4, letsencryptr4crosssigned, letsencrypte2, letsencryptauthorityx1, letsencryptx1crosssigned, letsencryptauthorityx2, letsencryptx2crosssigned, letsencryptauthorityx3, letsencryptx3crosssigned, letsencryptauthorityx4, letsencryptx4crosssigned, isrgrootocspx1, DigiCertBaltimoreRoot, CloudflareIncECCCA3)
 }
 
 func GetDir() string {
@@ -542,7 +586,7 @@ func GetDir() string {
 func TLSConfig() (tlsCfg *tls.Config) {
 	tlsCfg = &tls.Config{}
 	tlsCfg.MinVersion = tls.VersionTLS12
-	tlsCfg.RootCAs = loadCertPool(isrgrootx1, isrgrootx2, isrgrootx2crosssigned, letsencryptr3, letsencryptr3crosssigned, letsencrypte1, letsencryptr4, letsencryptr4crosssigned, letsencrypte2, letsencryptauthorityx1, letsencryptx1crosssigned, letsencryptauthorityx2, letsencryptx2crosssigned, letsencryptauthorityx3, letsencryptx3crosssigned, letsencryptauthorityx4, letsencryptx4crosssigned, isrgrootocspx1)
+	tlsCfg.RootCAs = loadCertPool(isrgrootx1, isrgrootx2, isrgrootx2crosssigned, letsencryptr3, letsencryptr3crosssigned, letsencrypte1, letsencryptr4, letsencryptr4crosssigned, letsencrypte2, letsencryptauthorityx1, letsencryptx1crosssigned, letsencryptauthorityx2, letsencryptx2crosssigned, letsencryptauthorityx3, letsencryptx3crosssigned, letsencryptauthorityx4, letsencryptx4crosssigned, isrgrootocspx1, DigiCertBaltimoreRoot, CloudflareIncECCCA3)
 
 	return
 }
